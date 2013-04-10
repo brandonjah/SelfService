72cea82c8f77840341d338fa0c3c52b0d1455e96
Brandon Jahner <brandon.jahner@arestravelinc.com>
2013-04-10 09:13:48 -0700

    finished moving logic to shared factory

--- public/app/layout.js
+++ public/app/layout.js
@@ -10,6 +10,7 @@ app.factory('saveObject', function($rootScope) {
 	    var sharedService = {};
 	    var dbLayoutObj = [];
     	var layoutCount = 0;
+    	var componentCount = 0;
 	    
 	    sharedService.prepForBroadcast = function() {
 	        this.broadcastItem();
@@ -19,17 +20,17 @@ app.factory('saveObject', function($rootScope) {
 	    	return dbLayoutObj;
 	    };
 	    
-	    sharedService.updateComponents = function(oper, componentId, className, containerName) {
+	    sharedService.updateComponents = function(oper, className, containerName) {
     		for (var i=0;i<dbLayoutObj.length;i++) {
-    			console.log('first');
     			if(containerName == dbLayoutObj[i].id) {
     				if(oper == 'add') {
-    					dbLayoutObj[i].components.push({'id':componentId,'className':className});
+    					dbLayoutObj[i].components.push({'id':componentCount,'className':className});
+    					componentCount++;
     				} else if(oper == 'del') {
     					for (var x = 0; x < dbLayoutObj[i].components.length; x++) {
-    						if(componentId == dbLayoutObj[i].components[x].id) {
+    						if(componentCount == dbLayoutObj[i].components[x].id) {
     							dbLayoutObj[i].components.splice(x, 1);
-//    							componentCount--;
+    							componentCount--;
     						}
     					}
     				}
@@ -51,7 +52,6 @@ app.factory('saveObject', function($rootScope) {
 	    			}
 	    		}
 	    	}
-	    	console.log(dbLayoutObj);
 	    };
 	   
 	    sharedService.broadcastItem = function() {
@@ -88,24 +88,12 @@ app.controller('layoutCtrl', function($scope, saveObject) {
 });
 
 app.controller('contentCtrl', function($scope, saveObject) {
-	var componentCount = 0;
-	$scope.containers = saveObject.getLayout();
-	$scope.components = [];
-
 	$scope.delComponent = function(thing) {
 		saveObject.updateComponents('del', thing.id, thing.className, thing.container);
-		for (var i = 0; i < $scope.components.length; i++) {
-			if(thing.id == $scope.components[i].id) {
-				this.components.splice(i, 1);
-				componentCount--;
-			}
-		}
 	};
 
 	$scope.dropCallback = function(event, ui) {
-		componentCount++; 
-		$scope.components.push({'id':componentCount, 'className' : ui.helper.context.className, 'container':$scope.item.id});
-		saveObject.updateComponents('add', componentCount, ui.helper.context.className, $scope.item.id);
+		saveObject.updateComponents('add', ui.helper.context.className, $scope.item.id);
 	};
 	
     $scope.$on('handleBroadcast', function() {
