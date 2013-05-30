3cd54987ea906c105bde324fc2764edb4c5c300f
Brandon Jahner <brandon.jahner@arestravelinc.com>
2013-05-30 15:56:14 -0700

    redesigning add content dropdown

---
+++ public/app/save-object.js
@@ -0,0 +1,81 @@
+app.factory('saveObject', function($routeParams) {
+	    var sharedService = {};
+	    var dbLayoutObj = [];
+	    dbLayoutObj.containers = [{"id":"Header0"},{"id":"Product1"},{"id":"Footer2"}];
+    	var containerCount = 3;
+    	var componentCount = 0;
+    	dbLayoutObj.siteId = $routeParams.siteId;
+	    
+	    sharedService.getLayout = function() {
+	    	return dbLayoutObj.containers;
+	    };
+	    
+	    sharedService.getSiteId = function() {
+	    	return dbLayoutObj.siteId;
+	    };
+	    
+	    sharedService.update = function(dbReturnObj) {
+	    	console.log("in shared service update");
+	    	console.log(dbReturnObj);
+	    	if(dbReturnObj.containers) {
+	    		console.log("not null");
+	    		dbLayoutObj = dbReturnObj;
+	    	} else {
+	    		console.log("null");
+	    		dbLayoutObj.containers = [];
+	    	}
+	    };
+	    
+	    sharedService.clear = function() {
+	    	dbLayoutObj.containers = [];
+	    };
+
+	    sharedService.updateComponents = function(oper, className, id, containerName, width) {
+    		for (var i=0;i<dbLayoutObj.containers.length;i++) {
+    				if((oper == 'add')&&(id == dbLayoutObj.containers[i].id)) {
+    					dbLayoutObj.containers[i].components.push({'id':componentCount,'className':className, 'width':width});
+    					componentCount++;
+    				} else if(oper == 'del') {
+    					for (var x = 0; x < dbLayoutObj.containers[i].components.length; x++) {
+    						if ((className == dbLayoutObj.containers[i].components[x].className)&&(id == dbLayoutObj.containers[i].components[x].id)) {
+    							dbLayoutObj.containers[i].components.splice(x, 1);
+								componentCount--;
+    						}
+    					}
+    				}
+    			}
+    		
+    		sharedService.updateComponentProperties = function(newComponent) {
+    			//@TODO find component by id then replace it newComponent
+    		}
+	    };
+	    
+	    sharedService.sidebarUpdateContainer = function(container) {
+	    	for (var i=0;i<dbLayoutObj.containers.length;i++) {
+	    		if(container.id == dbLayoutObj.containers[i].id) {
+					dbLayoutObj.containers[i] = container;
+	    		}
+	    	}
+	    };
+
+	    sharedService.updateContainers = function(id, oper) {
+	    	if(oper == 'add') {
+	    		dbLayoutObj.containers.push({'id':(id+containerCount),'components':[]});
+	    		containerCount++;
+	    	} else if (oper == 'del') {
+	    		for (var i = 0; i < dbLayoutObj.containers.length; i++) {
+	    			if(id == dbLayoutObj.containers[i].id) {
+	    				dbLayoutObj.containers.splice(i, 1);
+	    				containerCount--;
+	    			}
+	    		}
+	    	}
+	    };
+	    
+	    sharedService.logContents = function() {
+	    	console.log('dbLayoutObj');
+	    	console.log(dbLayoutObj);
+	    };
+
+	    return sharedService;
+	});
--- public/app/sidebar.js
+++ public/app/sidebar.js
@@ -1,3 +1,7 @@
+app.controller('sidebarContainerCtrl', function($scope, saveObject) {
+	$scope.containers = saveObject.getLayout();
+});
+
 app.controller('headerCtrl', function($scope) {
 	  $scope.items = [
 	                  "Full Widget",
@@ -8,15 +12,22 @@ app.controller('headerCtrl', function($scope) {
 });
 
 
-app.controller('productCtrl', function($scope) {
+app.controller('productCtrl', function($scope, saveObject) {	
+	$scope.setContainer = function(container) {
+		console.log("setContainer");
+		console.log(container);
+		console.log($scope.className);
+		container.className = $scope.className;
+		saveObject.sidebarUpdateContainer(container);
+	}
 	$scope.selectedProduct = "Add Content";
 	$scope.widget = false;
 	  $scope.widgetTabs = {
-			    hotel: false,
+			    hotel: true,
 			    deal: true,
-			    car: false,
-			    ticket: false,
-			    flight: false
+			    car: true,
+			    ticket: true,
+			    flight: true
 			  };
 	$scope.image = false;
 	$scope.text = false;
@@ -24,96 +35,77 @@ app.controller('productCtrl', function($scope) {
 	$scope.attraction = false;
 	$scope.deal = false;
 	$scope.classname;
+	$scope.widths = [
+	                 {"text":"1/4","id":"14"},
+	                 {"text":"3/4","id":"34"},
+	                 {"text":"1/2","id":"12"},
+	                 {"text":"Full","id":"1"}
+	                 ];
+	$scope.align = ["left","right"];
 	  $scope.items = [
-	                  {"text":"Full Widget","enabled":"w","className":"fw"},
-	                  {"text":"Full Image","enabled":"i","className":"fi"},
-	                  {"text":"Full Text","enabled":"t","className":"ft"},
-	                  {"text":"Widget + 3/4 Image","enabled":"wi","className":"w34i"},
-	                  {"text":"Widget + 1/4 Image","enabled":"wi","className":"w14i"},
-	                  {"text":"Widget + 1/2 Image","enabled":"wi","className":"w12i"},
-	                  {"text":"Widget + 3/4 Text","enabled":"wt","className":"w34t"},
-	                  {"text":"Widget + 1/4 Text","enabled":"wt","className":"w14t"},
-	                  {"text":"Widget + 1/2 Text","enabled":"wt","className":"w12t"},
-	                  {"text":"Text + 3/4 Image","enabled":"ti","className":"t34i"},
-	                  {"text":"Text + 1/4 Image","enabled":"ti","className":"t14i"},
+	                  {"text":"Widget","enabled":"w","className":"fw"},
+	                  {"text":"Image","enabled":"i","className":"fi"},
+	                  {"text":"Text","enabled":"t","className":"ft"},
+	                  {"text":"Widget + Image","enabled":"wi","className":"w34i"},
+	                  {"text":"Widget + Text","enabled":"wt","className":"w34t"},
+	                  {"text":"Text + Image","enabled":"ti","className":"t34i"},
 	                  {"text":"Hotel","enabled":"h","className":"hotel"},
 	                  {"text":"Attraction","enabled":"a","className":"attraction"},
-	                  {"text":"Deals","enabled":"d","className":"deal"},
+	                  {"text":"Deals","enabled":"d","className":"deal"}
+//	                  {"text":"Full Widget","enabled":"w","className":"fw"},
+//	                  {"text":"Full Image","enabled":"i","className":"fi"},
+//	                  {"text":"Full Text","enabled":"t","className":"ft"},
+//	                  {"text":"Widget + 3/4 Image","enabled":"wi","className":"w34i"},
+//	                  {"text":"Widget + 1/4 Image","enabled":"wi","className":"w14i"},
+//	                  {"text":"Widget + 1/2 Image","enabled":"wi","className":"w12i"},
+//	                  {"text":"Widget + 3/4 Text","enabled":"wt","className":"w34t"},
+//	                  {"text":"Widget + 1/4 Text","enabled":"wt","className":"w14t"},
+//	                  {"text":"Widget + 1/2 Text","enabled":"wt","className":"w12t"},
+//	                  {"text":"Text + 3/4 Image","enabled":"ti","className":"t34i"},
+//	                  {"text":"Text + 1/4 Image","enabled":"ti","className":"t14i"},
+//	                  {"text":"Hotel","enabled":"h","className":"hotel"},
+//	                  {"text":"Attraction","enabled":"a","className":"attraction"},
+//	                  {"text":"Deals","enabled":"d","className":"deal"}
 	                ];
 	  $scope.selection = function(choice) {
 		  $scope.selectedProduct = choice.text;
+		  $scope.className = choice.className;
+		  $scope.widget = false;
+		  $scope.image = false;
+		  $scope.text = false;
+			$scope.hotel = false;
+			$scope.attraction = false;
+			$scope.deal = false;
 		  switch (choice.enabled)
 		  {
 		  case "w":
 			  $scope.widget = true;
-			  $scope.image = false;
-			  $scope.text = false;
-				$scope.hotel = false;
-				$scope.attraction = false;
-				$scope.deal = false;
 		    break;
 		  case "i":
 			  $scope.image = true;
-			  $scope.widget = false;
-			  $scope.text = false;
-				$scope.hotel = false;
-				$scope.attraction = false;
-				$scope.deal = false;
 		    break;
 		  case "t":
 			  $scope.text = true;
-			  $scope.image = false;
-			  $scope.widget = false;
-				$scope.hotel = false;
-				$scope.attraction = false;
-				$scope.deal = false;
 		    break;
 		  case "wi":
 			  $scope.image = true;
 			  $scope.widget = true;
-			  $scope.text = false;
-				$scope.hotel = false;
-				$scope.attraction = false;
-				$scope.deal = false;
 		    break;
 		  case "wt":
 			  $scope.text = true;
 			  $scope.widget = true;
-			  $scope.image = false;
-				$scope.hotel = false;
-				$scope.attraction = false;
-				$scope.deal = false;
 		    break;
 		  case "ti":
 			  $scope.text = true;
 			  $scope.image = true;
-			  $scope.widget = false;
-				$scope.hotel = false;
-				$scope.attraction = false;
-				$scope.deal = false;
 		    break;
 		  case "h":
-			  $scope.text = false;
-			  $scope.image = false;
-			  $scope.widget = false;
 				$scope.hotel = true;
-				$scope.attraction = false;
-				$scope.deal = false;
 		    break;
 		  case "a":
-			  $scope.text = false;
-			  $scope.image = false;
-			  $scope.widget = false;
-				$scope.hotel = false;
 				$scope.attraction = true;
-				$scope.deal = false;
 		    break;
 		  case "d":
-			  $scope.text = false;
-			  $scope.image = false;
-			  $scope.widget = false;
-				$scope.hotel = false;
-				$scope.attraction = false;
 				$scope.deal = true;
 		    break;
 		  }
